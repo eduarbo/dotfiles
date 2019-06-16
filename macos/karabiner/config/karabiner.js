f82baf365d608d39d5b79eb9bf8e14dcc395aeb3
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-06-16 12:18:53 -0500

    add rev2 of qwerty & symbols layers

--- macos/karabiner/config/karabiner.js
+++ macos/karabiner/config/karabiner.js
@@ -6,7 +6,9 @@ const emacs = require('./mods/emacs');
 const fn = require('./mods/fn');
 const launcher = require('./mods/launcher');
 const qwerty = require('./mods/qwerty');
+const qwertyRev2 = require('./mods/qwerty-rev2');
 const symbols = require('./mods/symbols');
+const symbolsRev2 = require('./mods/symbols-rev2');
 
 const karabiner = {
   profiles: [
@@ -19,13 +21,22 @@ const karabiner = {
       qwerty,
       launcher,
     ], {
-      selected: true,
       devices: [{
         identifiers: corneKeyboard,
         disable_built_in_keyboard_if_exists: false,
         ignore: false,
         manipulate_caps_lock_led: true,
       }],
+    }),
+    profile('Rev 2', [
+      arrows,
+      emacs,
+      fn,
+      symbolsRev2,
+      qwertyRev2,
+      launcher,
+    ], {
+      selected: true,
     }),
   ],
   global: {
@@ -41,7 +52,8 @@ const complexModifications = {
   fn,
   launcher,
   qwerty,
-  symbols,
+  qwertyRev2,
+  symbolsRev2,
 };
 
 module.exports = {
---
+++ macos/karabiner/config/mods/symbols-rev2.js
@@ -0,0 +1,40 @@
+const { remap } = require('../../utils');
+
+const mandatoryMods = ['left_shift'];
+const optionalMods = ['caps_lock', 'command', 'control', 'option'];
+const shiftedNumbersRow = 'pqwertyuio'.split('');
+const numbersRow = ['semicolon', ...'asdfghjkl'.split('')];
+const remapToLayer = (keyCode, toKey) => remap([keyCode, mandatoryMods, optionalMods], toKey);
+
+module.exports = {
+  title: 'Symbols layer',
+  rules: [
+    {
+      description: 'Shifted numbers in upper row, numbers in home row, other symbols in lower row',
+      manipulators: [
+        // Upper row
+        ...shiftedNumbersRow.map((key, num) => remapToLayer(key, [[num.toString(), ['left_shift']]])),
+
+        // Home row
+        ...numbersRow.map((key, num) => remapToLayer(key, [[num.toString()]])),
+        remapToLayer('caps_lock', [['f13']]),
+        remapToLayer('quote', [['backslash', ['left_shift']]]),
+
+        // Lower row
+        remapToLayer('left_shift', [['1', ['left_option']]]),
+        remapToLayer('grave_accent_and_tilde', [['1', ['left_option']]]),
+        remapToLayer('z', [['hyphen', ['left_shift']]]),
+        remapToLayer('x', [['hyphen']]),
+        remapToLayer('c', [['equal_sign', ['left_shift']]]),
+        remapToLayer('v', [['equal_sign']]),
+        remapToLayer('b', [['backslash']]),
+        remapToLayer('n', [['n', ['left_option']]]),
+        remapToLayer('m', [['e', ['left_option']]]),
+        remapToLayer('comma', [['comma']]),
+        remapToLayer('period', [['period']]),
+        remapToLayer('slash', [['slash']]),
+        remapToLayer('right_shift', [['slash', ['left_shift', 'left_option']]]),
+      ],
+    },
+  ],
+};
