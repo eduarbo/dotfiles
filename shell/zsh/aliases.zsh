c9f566d97948447bfd4217eb48a7b3b28ea6ca23
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-07-03 12:13:56 -0500

    add server alias

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -123,6 +123,9 @@ alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pa
 # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
 
+# quick way to serve a directory, very handy
+alias server='python -m SimpleHTTPServer'
+
 zman() { PAGER="less -g -s '+/^       "$1"'" man zshall; }
 
 r() {
