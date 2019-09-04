df2691c4dee883d87737a839f9be5b4850829339
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-04 01:40:21 -0500

    deprecate Rev 1 and give profile a yummy name

--- misc/karabiner/config/mods/qwerty.js
+++ misc/karabiner/config/mods/qwerty.js
@@ -1,4 +1,21 @@
-const { modTap, remap } = require('../../utils');
+const utils = require('../../utils');
+const { corneKeyboard } = require('../devices');
+
+const remap = (fromKey, toKey, options) => utils.remap(fromKey, toKey, {
+  // conditions: [{
+  //   type: 'device_unless',
+  //   identifiers: [corneKeyboard],
+  // }],
+  // ...options,
+});
+
+const modTap = (fromKey, toKey, toKeyOnTap, options) => utils.modTap(fromKey, toKey, toKeyOnTap, {
+  // conditions: [{
+  //   type: 'device_unless',
+  //   identifiers: [corneKeyboard],
+  // }],
+  // ...options,
+});
 
 module.exports = {
   title: 'Qwerty layer',
@@ -9,19 +26,21 @@ module.exports = {
         modTap(['spacebar'], [['right_command']], [['return_or_enter']]),
         modTap(['right_command'], [['right_shift']], [['escape']]),
         modTap(['left_command'], [['left_shift']], [['spacebar']]),
-        modTap(['right_option'], [['right_option']], [['0', ['left_shift']]]),
-        modTap(['left_option'], [['right_control']], [['9', ['left_shift']]]),
+        // sticky(['left_command', ['caps_lock', 'command', 'control', 'option']], 'symbols_layer'),
+        modTap(['right_option'], [['right_option']], [['escape']]),
+        modTap(['left_option'], [['right_control']], [['spacebar']]),
       ],
     },
     {
       description: 'Custom Qwerty',
       manipulators: [
         remap(['delete_or_backspace'], [['f13']]),
-        remap(['open_bracket'], [['delete_or_backspace']]),
-        remap(['caps_lock'], [['open_bracket']]),
-        remap(['quote'], [['close_bracket']]),
-        remap(['left_shift'], [['grave_accent_and_tilde']]),
-        remap(['right_shift'], [['quote']]),
+        remap(['open_bracket'], [['grave_accent_and_tilde']]),
+        remap(['caps_lock', ['command']], [['grave_accent_and_tilde', ['left_command']]]),
+        remap(['caps_lock'], [['delete_or_backspace']]),
+        remap(['quote'], [['quote']]),
+        remap(['left_shift'], [['open_bracket']]),
+        remap(['right_shift'], [['close_bracket']]),
       ],
     },
   ],
