d7773b5df8b63f6a3ac6b07c43fa9c54d1bb24eb
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-09 18:02:47 -0500

    vim: new vim+fzf alias and command to save as sudo

--- editor/vim/aliases.bash
+++ editor/vim/aliases.bash
@@ -1,1 +1,6 @@
-alias v='vim'
+# fasd & fzf change directory - open best matched file using `fasd` if given argument, filter output of `fasd` using `fzf` else
+_is_callable fzf && v() {
+    [ $# -gt 0 ] && fasd -f -e ${EDITOR} "$*" && return
+    local file
+    file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && vi "${file}" || return 1
+}
