8f39fa00032dd07da535736e9e3d83c79dddbb9b
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-09 12:36:52 -0500

    add comment to skim jump functions

--- shell/skim/aliases.bash
+++ shell/skim/aliases.bash
@@ -4,13 +4,14 @@ alias skgl='sk-git-log'
 alias bm='sk-browser-bookmarks'
 alias hist='sk-browser-history'
 
+# TODO --select-1 and --exit-0 are not yet implemented, so it won't work as FZF does
 j() {
   local dir
-  dir="$(fasd -Rdl "$1" | sk -1 -0 --no-sort)" && cd "${dir}" || return 1
+  dir="$(fasd -Rdl "$1" | sk --select-1 --exit-0 --no-sort)" && cd "${dir}" || return 1
 }
 
 # change directory
 jj() {
   local dir
-  dir="$(fd --type directory --follow --hidden "$1" | fzf -1 -0 --no-sort)" && cd "${dir}" || return 1
+  dir="$(fd --type directory --follow --hidden "$1" | fzf --select-1 --exit-0 --no-sort)" && cd "${dir}" || return 1
 }
