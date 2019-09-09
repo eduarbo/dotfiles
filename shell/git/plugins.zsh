03af218cd93494d88b12444eeed6f632f1a59a81
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-08 22:09:36 -0500

    fzf: reload shell to install zplugins

--- shell/git/plugins.zsh
+++ shell/git/plugins.zsh
@@ -1,2 +1,2 @@
-zpl ice wait"2" lucid as"program" pick"bin/git-dsf"; zpl light zdharma/zsh-diff-so-fancy
-zpl ice wait"2" lucid as"program" pick"bin/git-*"; zpl light tj/git-extras
+zplugin ice wait"2" lucid as"program" pick"bin/git-dsf"; zpl light zdharma/zsh-diff-so-fancy
+zplugin ice wait"2" lucid as"program" pick"bin/git-*"; zpl light tj/git-extras
