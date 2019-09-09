c7e7f6d3f22cbb57b8335b9e62ba4efd5083f947
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-09 00:17:28 -0500

    aadd skim/bin to the PATH

--- shell/skim/env.bash
+++ shell/skim/env.bash
@@ -1,1 +1,4 @@
+export SKIM_DIR="$XDG_CONFIG_HOME/skim"
 export SKIM_DEFAULT_OPTIONS='--reverse --color=16,fg+:2,bg+:0,hl:4,hl+:4,prompt:4,pointer:8'
+
+[[ $(_os) == debian ]] && path=( "$SKIM_DIR/bin" "${path[@]}" )
