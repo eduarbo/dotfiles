052f5379735c1192b01f572ae0f76c2d4f6cfe22
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-08 22:51:40 -0500

    skim: add clean function

--- shell/sk/aliases.bash
+++ shell/sk/aliases.bash
@@ -3,3 +3,14 @@ alias skgl='sk-git-log'
 
 alias bm='sk-browser-bookmarks'
 alias hist='sk-browser-history'
+
+j() {
+  local dir
+  dir="$(fasd -Rdl "$1" | sk -1 -0 --no-sort)" && cd "${dir}" || return 1
+}
+
+# change directory
+jj() {
+  local dir
+  dir="$(fd --type directory --follow --hidden "$1" | fzf -1 -0 --no-sort)" && cd "${dir}" || return 1
+}
