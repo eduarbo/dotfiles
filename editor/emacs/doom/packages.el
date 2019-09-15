401739d0926d39156ef12eec322614c01e813929
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-15 03:59:33 -0500

    remove annoying emmet-mode

--- editor/emacs/doom/packages.el
+++ editor/emacs/doom/packages.el
@@ -3,4 +3,6 @@
 
 ;; The ESC key is reachable in my custom keyboard layout so I don't need this
 (package! evil-escape :disable t)
+;; Very annoying when trying to autocomplete with company, fuck off!
+(package! emmet-mode :disable t)
 (package! org-journal)
