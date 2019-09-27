5abe8ee0efd92f40b418e7d826ffe77a1c760ac6
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-27 10:25:18 -0500

    surfingkeys: enforce code consistency with eslint

---
+++ misc/surfingkeys/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-use-before-define': ['error', { functions: false }],
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }]
+  },
+  globals: {
+    settings: true,
+    Hints: true,
+    mapkey: true,
+    vmapkey: true,
+    imapkey: true,
+    map: true,
+    unmap: true,
+    iunmap: true,
+    tabOpenLink: true,
+    Front: true,
+    runtime: true,
+    RUNTIME: true,
+    Normal: true,
+  },
+  env: {
+    browser: true,
+  }
+};
