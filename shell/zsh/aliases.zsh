4314f349224dd3304d35570e317b937849852048
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-09-28 04:50:04 -0500

    zsh: prettify zshrc headings

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -6,11 +6,6 @@ autoload -U zmv
 
 zman() { PAGER="less -g -s '+/^       "$1"'" man zshall; }
 
-r() {
-  local time=$1; shift
-  sched "$time" "notify-send --urgency=critical 'Reminder' '$@'; ding";
-}; compdef r=sched
-
 
 ## FZF
 
