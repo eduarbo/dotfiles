c58840a21cbaa7545e7ded7bb7ac4824aa7bd438
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-10-01 01:48:26 -0500

    doom: enable format pkg

--- editor/emacs/doom/init.el
+++ editor/emacs/doom/init.el
@@ -57,7 +57,9 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;; (format +onsave)  ; automated prettiness
+       (format           ; automated prettiness
+        ;; +onsave
+        )
        ;; lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
        ;; objed             ; text object editing for the innocent
