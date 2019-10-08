e25535d8af691c82aec793df0e67a7a69fdb7f11
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-10-07 21:25:52 -0500

    🔨🥄 Hammerspoon: general cleanup & fixes

--- macos/hammerspoon/config/window.lua
+++ macos/hammerspoon/config/window.lua
@@ -46,55 +46,55 @@ prefix.bind('', 'g', function() hs.grid.show() end)
 
 local arrowKeys = {'h', 'j', 'k', 'l'}
 local rectMap = {
-    ['h'] = {0, 0, 0.5, 1},
-    ['j'] = {0, 0.5, 1, 0.5},
-    ['k'] = {0, 0, 1, 0.5},
-    ['l'] = {0.5, 0, 0.5, 1},
-    ['hj'] = {0, 0.5, 0.5, 0.5},
-    ['hk'] = {0, 0, 0.5, 0.5},
-    ['jl'] = {0.5, 0.5, 0.5, 0.5},
-    ['kl'] = {0.5, 0, 0.5, 0.5},
-    ['hl'] = {0, 0, 1, 1},
+  ['h'] = {0, 0, 0.5, 1},
+  ['j'] = {0, 0.5, 1, 0.5},
+  ['k'] = {0, 0, 1, 0.5},
+  ['l'] = {0.5, 0, 0.5, 1},
+  ['hj'] = {0, 0.5, 0.5, 0.5},
+  ['hk'] = {0, 0, 0.5, 0.5},
+  ['jl'] = {0.5, 0.5, 0.5, 0.5},
+  ['kl'] = {0.5, 0, 0.5, 0.5},
+  ['hl'] = {0, 0, 1, 1},
 }
 local wasPressed = {false, false, false, false}
 local pressed = {false, false, false, false}
 
 local function resizeWindow()
-    for i = 1, #pressed do
-        if pressed[i] then
-            return
-        end
+  for i = 1, #pressed do
+    if pressed[i] then
+      return
     end
+  end
 
-    local win = hs.window.focusedWindow()
-    if win ~= nil then
-        local keys = ''
-        for i = 1, #wasPressed do
-            if wasPressed[i] then
-                keys = keys .. arrowKeys[i]
-                wasPressed[i] = false
-            end
-        end
-        local rect = rectMap[keys]
-        if rect ~= nil then
-            win:move(rect)
-        elseif keys == 'jk' then
-            win:centerOnScreen()
-        end
+  local win = hs.window.focusedWindow()
+  if win ~= nil then
+    local keys = ''
+    for i = 1, #wasPressed do
+      if wasPressed[i] then
+        keys = keys .. arrowKeys[i]
+        wasPressed[i] = false
+      end
     end
-    prefix.exit()
+    local rect = rectMap[keys]
+    if rect ~= nil then
+      win:move(rect)
+    elseif keys == 'jk' then
+      win:centerOnScreen()
+    end
+  end
+  prefix.exit()
 end
 
 for i = 1, #arrowKeys do
-    local pressedFn = function()
-        wasPressed[i] = true
-        pressed[i] = true
-    end
-    local releasedFn = function()
-        pressed[i] = false
-        resizeWindow()
-    end
-    prefix.bindMultiple('', arrowKeys[i], pressedFn, releasedFn, nil)
+  local pressedFn = function()
+    wasPressed[i] = true
+    pressed[i] = true
+  end
+  local releasedFn = function()
+    pressed[i] = false
+    resizeWindow()
+  end
+  prefix.bindMultiple('', arrowKeys[i], pressedFn, releasedFn, nil)
 end
 
 -- prefix + ctrl-h -> left one third
@@ -103,20 +103,20 @@ end
 -- prefix + ctrl-l -> right one third
 
 local rectMapCtrl = {
-    ['h'] = {0, 0, 1/3, 1},
-    ['j'] = {0, 0, 2/3, 1},
-    ['k'] = {1/3, 0, 2/3, 1},
-    ['l'] = {2/3, 0, 1/3, 1},
+  ['h'] = {0, 0, 1/3, 1},
+  ['j'] = {0, 0, 2/3, 1},
+  ['k'] = {1/3, 0, 2/3, 1},
+  ['l'] = {2/3, 0, 1/3, 1},
 }
 
 for k, v in pairs(rectMapCtrl) do
-    local fn = function()
-        win = hs.window.focusedWindow()
-        if win ~= nil then
-            win:move(v)
-        end
+  local fn = function()
+    win = hs.window.focusedWindow()
+    if win ~= nil then
+      win:move(v)
     end
-    prefix.bind('ctrl', k, fn)
+  end
+  prefix.bind('ctrl', k, fn)
 end
 
 -- prefix + shift-hjkl -> move window
@@ -126,43 +126,43 @@ local DY = {0, 1, -1, 0}
 local DELTA = 20
 
 for i = 1, 4 do
-    local moveWin = function()
-        local win = hs.window.focusedWindow()
-        if win ~= nil then
-            local p = win:topLeft()
-            p.x = p.x + DX[i] * DELTA
-            p.y = p.y + DY[i] * DELTA
-            win:setTopLeft(p)
-        end
+  local moveWin = function()
+    local win = hs.window.focusedWindow()
+    if win ~= nil then
+      local p = win:topLeft()
+      p.x = p.x + DX[i] * DELTA
+      p.y = p.y + DY[i] * DELTA
+      win:setTopLeft(p)
     end
-    local pressedFn = function()
-        prefix.cancelTimeout()
-        moveWin()
-    end
-    prefix.bindMultiple('shift', arrowKeys[i], pressedFn, nil, moveWin)
+  end
+  local pressedFn = function()
+    prefix.cancelTimeout()
+    moveWin()
+  end
+  prefix.bindMultiple('shift', arrowKeys[i], pressedFn, nil, moveWin)
 end
 
 -- prefix + tab -> move window to the next screen
 
 local function getNextScreen(s)
-    all = hs.screen.allScreens()
-    for i = 1, #all do
-        if all[i] == s then
-            return all[(i - 1 + 1) % #all + 1]
-        end
+  all = hs.screen.allScreens()
+  for i = 1, #all do
+    if all[i] == s then
+      return all[(i - 1 + 1) % #all + 1]
     end
-    return nil
+  end
+  return nil
 end
 
 local function moveToNextScreen()
-    local win = hs.window.focusedWindow()
-    if win ~= nil then
-        currentScreen = win:screen()
-        nextScreen = getNextScreen(currentScreen)
-        if nextScreen then
-            win:moveToScreen(nextScreen)
-        end
+  local win = hs.window.focusedWindow()
+  if win ~= nil then
+    currentScreen = win:screen()
+    nextScreen = getNextScreen(currentScreen)
+    if nextScreen then
+      win:moveToScreen(nextScreen)
     end
+  end
 end
 
 prefix.bind('', 'tab', moveToNextScreen)
@@ -177,19 +177,18 @@ prefix.bind('', 'tab', moveToNextScreen)
 -- prefix + = -> expand window frame
 
 local function expandWin(ratio)
-    local win = hs.window.focusedWindow()
-    if win == nil then
-        return
-    end
-    frame = win:frame()
-    local cx = frame.x + frame.w / 2
-    local cy = frame.y + frame.h / 2
-    local nw = frame.w * ratio
-    local nh = frame.h * ratio
-    local nx = cx - nw / 2
-    local ny = cy - nh / 2
-    win:setFrame(hs.geometry.rect(nx, ny, nw, nh))
-    redrawBorder()
+  local win = hs.window.focusedWindow()
+  if win == nil then
+    return
+  end
+  frame = win:frame()
+  local cx = frame.x + frame.w / 2
+  local cy = frame.y + frame.h / 2
+  local nw = frame.w * ratio
+  local nh = frame.h * ratio
+  local nx = cx - nw / 2
+  local ny = cy - nh / 2
+  win:setFrame(hs.geometry.rect(nx, ny, nw, nh))
 end
 
 local function bindExpandWin(ratio)
@@ -218,118 +217,40 @@ prefix.bindMultiple('', '=', pressedExpandWin(1.1), nil, bindExpandWin(1.1))
 -- prefix + cmd-shift-hjkl -> shrink window edges
 
 local function expandEdge(edge, ratio)
-    local win = hs.window.focusedWindow()
-    if win == nil then
-        return
-    end
-    frame = win:frame()
-    local x, y, w, h = frame.x, frame.y, frame.w, frame.h
-    if edge == 'h' then
-        w = frame.w * ratio
-        x = frame.x + frame.w - w
-    elseif edge == 'j' then
-        h = frame.h * ratio
-    elseif edge == 'k' then
-        h = frame.h * ratio
-        y = frame.y + frame.h - h
-    elseif edge == 'l' then
-        w = frame.w * ratio
-    else
-        return
-    end
-    win:setFrame(hs.geometry.rect(x, y, w, h))
-    redrawBorder()
+  local win = hs.window.focusedWindow()
+  if win == nil then
+    return
+  end
+  frame = win:frame()
+  local x, y, w, h = frame.x, frame.y, frame.w, frame.h
+  if edge == 'h' then
+    w = frame.w * ratio
+    x = frame.x + frame.w - w
+  elseif edge == 'j' then
+    h = frame.h * ratio
+  elseif edge == 'k' then
+    h = frame.h * ratio
+    y = frame.y + frame.h - h
+  elseif edge == 'l' then
+    w = frame.w * ratio
+  else
+    return
+  end
+  win:setFrame(hs.geometry.rect(x, y, w, h))
 end
 
 local edges = {'h', 'j', 'k', 'l'}
 local ratios = {0.9, 1.111111}
 
 for i = 1, #edges do
-    local edge = edges[i]
-    for j = 1, #ratios do
-        local mod = (ratios[j] > 1) and 'cmd' or 'cmd+shift'
-        local fn = function() expandEdge(edge, ratios[j]) end
-        local pressedFn = function()
-            prefix.cancelTimeout()
-            fn()
-        end
-        prefix.bindMultiple(mod, edge, pressedFn, nil, fn)
+  local edge = edges[i]
+  for j = 1, #ratios do
+    local mod = (ratios[j] > 1) and 'cmd' or 'cmd+shift'
+    local fn = function() expandEdge(edge, ratios[j]) end
+    local pressedFn = function()
+      prefix.cancelTimeout()
+      fn()
     end
-end
-
-
--- ╻ ╻╻┏┓╻╺┳┓┏━┓╻ ╻   ┏┓ ┏━┓┏━┓╺┳┓┏━╸┏━┓
--- ┃╻┃┃┃┗┫ ┃┃┃ ┃┃╻┃   ┣┻┓┃ ┃┣┳┛ ┃┃┣╸ ┣┳┛
--- ┗┻┛╹╹ ╹╺┻┛┗━┛┗┻┛   ┗━┛┗━┛╹┗╸╺┻┛┗━╸╹┗╸
--- Window Border
-
-showborders = true
-
--- https://www.uicolor.xyz/#/hex-to-ui to convert HEX to RGB
-local GREEN_COLOR = {red=0.74, green=0.91, blue=0.26, alpha=0.8} -- green
-local YELLOW_COLOR = {red=1, green=0.81, blue=0.28, alpha=0.8} -- yellow
-local EMERALD_COLOR = {red=0.14, green=0.87, blue=0.70, alpha=0.8} -- emerald
-
-local borderColor = YELLOW_COLOR
-local borderWidth = 4
-
-prefix.bind('', 'b', function() toggleBorders() end)
-
-----
-
-local currentIndicator
-
-function toggleBorders()
-  showborders = not showborders
-
-  if showborders then
-    redrawBorder()
-    currentIndicator:show()
-  else
-    currentIndicator:hide()
+    prefix.bindMultiple(mod, edge, pressedFn, nil, fn)
   end
 end
-
-function redrawBorder()
-  if currentIndicator ~= nil then
-    currentIndicator:delete()
-    -- currentIndicator:hide()
-  end
-
-  local win = hs.window.focusedWindow()
-
-  if win ~= nil and showborders then
-    local winapp  = win:application():name()
-
-    local excludedApps = {
-      ['iTerm2']=true,
-      ['Spotlight']=true,
-      ['Alfred']=true
-    }
-
-    if not excludedApps[winapp] then
-      local size = win:size()
-      local topLeft = win:topLeft()
-      local activeWindowRect = hs.geometry.rect(topLeft.x, topLeft.y, size.w, size.h)
-
-      currentIndicator = hs.drawing.rectangle(activeWindowRect)
-
-      currentIndicator:setStrokeColor(borderColor)
-      currentIndicator:setFill(false)
-      currentIndicator:setStrokeWidth(borderWidth)
-      currentIndicator:setRoundedRectRadii(5.0, 5.0)
-      currentIndicator:setLevel("dock")
-      currentIndicator:show()
-    end
-  end
-end
-
-redrawBorder()
-
-allwindows = hs.window.filter.new(nil)
-allwindows:subscribe(hs.window.filter.windowCreated, function () redrawBorder() end)
-allwindows:subscribe(hs.window.filter.windowFocused, function () redrawBorder() end)
-allwindows:subscribe(hs.window.filter.windowMoved, function () redrawBorder() end)
-allwindows:subscribe(hs.window.filter.windowUnfocused, function () redrawBorder() end)
-allwindows:subscribe(hs.window.filter.windowVisible, function () redrawBorder() end)
-allwindows:subscribe(hs.window.filter.hasNoWindows, function () redrawBorder() end)
