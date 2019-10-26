774d6420a7128e0889dc6ae3f8491324878832fa
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-10-26 02:50:27 -0500

    change pinentry for SSH logins

--- shell/bash/env.bash
+++ shell/bash/env.bash
@@ -5,3 +5,12 @@ export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu
 export PAGER=less
 export LESS='-R -i -w -M -z-4'
 export LESSHISTFILE="$XDG_DATA_HOME/lesshst"
+
+# Changing pinentry for SSH logins
+
+GPG_TTY=$(tty)
+export GPG_TTY
+
+if [[ -n "$SSH_CONNECTION" ]] ;then
+  export PINENTRY_USER_DATA="USE_CURSES=1"
+fi
