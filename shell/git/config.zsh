dd07b4988977d60ebd52908d820788183d212649
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-10-27 11:03:18 -0600

    zsh: do not load git plugins on dumb $TERMs

--- shell/git/config.zsh
+++ shell/git/config.zsh
@@ -1,2 +1,4 @@
-zplugin ice wait"2" lucid as"program" pick"bin/git-dsf"; zpl light zdharma/zsh-diff-so-fancy
-zplugin ice wait"2" lucid as"program" pick"bin/git-*"; zpl light tj/git-extras
+if [[ "$TERM" != "dumb" ]]; then
+  zplugin ice wait"2" lucid as"program" pick"bin/git-dsf"; zpl light zdharma/zsh-diff-so-fancy
+  zplugin ice wait"2" lucid as"program" pick"bin/git-*"; zpl light tj/git-extras
+fi
