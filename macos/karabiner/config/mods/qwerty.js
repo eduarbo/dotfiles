8e6eb9a76d9c73584e825fca71519d8c986a828e
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-12-27 08:52:30 -0600

    karabiner: move to eduarbo v3

--- macos/karabiner/config/mods/qwerty.js
+++ macos/karabiner/config/mods/qwerty.js
@@ -1,24 +1,10 @@
 const utils = require('../../utils');
-const { corneKeyboard } = require('../devices');
 
-const remap = (fromKey, toKey, options) => utils.remap(fromKey, toKey, {
-  // conditions: [{
-  //   type: 'device_unless',
-  //   identifiers: [corneKeyboard],
-  // }],
-  // ...options,
-});
-
-const modTap = (fromKey, toKey, toKeyOnTap, options) => utils.modTap(fromKey, toKey, toKeyOnTap, {
-  // conditions: [{
-  //   type: 'device_unless',
-  //   identifiers: [corneKeyboard],
-  // }],
-  // ...options,
-});
+const remap = (fromKey, toKey) => utils.remap(fromKey, toKey);
+const modTap = (fromKey, toKey, toKeyOnTap) => utils.modTap(fromKey, toKey, toKeyOnTap);
 
 module.exports = {
-  title: 'Qwerty layer',
+  title: 'Alternative Qwerty layer',
   rules: [
     {
       description: 'Thumb cluster',
@@ -26,23 +12,35 @@ module.exports = {
         modTap(['spacebar'], [['right_command']], [['return_or_enter']]),
         modTap(['right_command'], [['right_shift']], [['escape']]),
         modTap(['left_command'], [['left_shift']], [['spacebar']]),
-        // sticky(['left_command', ['caps_lock', 'command', 'control', 'option']], 'symbols_layer'),
-        modTap(['right_option'], [['right_option']], [['escape']]),
-        modTap(['left_option'], [['right_control']], [['spacebar']]),
+
+        modTap(['right_option'], [['right_option']], [['return_or_enter']]),
+        modTap(['left_option'], [['right_control']], [['escape']]),
+      ],
+    },
+    {
+      description: 'Custom Qwerty Tweaks',
+      manipulators: [
+        // Swap CMD+LSHIFT <-> CMD+` to swich between apps/windows with one hand
+        remap(['left_shift', ['command']], [['grave_accent_and_tilde', ['left_command']]]),
+        remap(['open_bracket', ['command']], [['delete_or_backspace', ['left_command']]]),
+
+        // Quick access to F13 for the built-in macbook keyboard
+        remap(['delete_or_backspace'], [['f13']]),
       ],
     },
     {
       description: 'Custom Qwerty',
       manipulators: [
-        // Swap CMD+DEL <-> CMD+`
-        remap(['caps_lock', ['command']], [['grave_accent_and_tilde', ['left_command']]]),
-        remap(['open_bracket', ['command']], [['delete_or_backspace', ['left_command']]]),
-
-        remap(['delete_or_backspace'], [['f13']]),
-        remap(['open_bracket'], [['grave_accent_and_tilde']]),
-        remap(['caps_lock'], [['delete_or_backspace']]),
-        remap(['left_shift'], [['open_bracket']]),
-        remap(['right_shift'], [['close_bracket']]),
+        // [ -> '
+        remap(['open_bracket'], [['quote']]),
+        // ' -> ]
+        remap(['quote'], [['close_bracket']]),
+        // CAPS -> [
+        remap(['caps_lock'], [['open_bracket']]),
+        // LSHFIT -> DEL
+        remap(['left_shift'], [['delete_or_backspace']]),
+        // RSHIFT -> ´
+        remap(['right_shift'], [['e', ['left_option']]]),
       ],
     },
   ],
