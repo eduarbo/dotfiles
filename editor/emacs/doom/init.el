0ab1258345885a2dc5031555a835f6116e9ac0d9
Eduardo Ruiz Macias <eduarbo@gmail.com>
2020-06-08 10:55:43 -0500

    emacs: enable roam

--- editor/emacs/doom/init.el
+++ editor/emacs/doom/init.el
@@ -160,6 +160,7 @@
          ;; +pomodoro       ; be fruitful with the tomato technique
          +present        ; using org-mode for presentations
          +journal
+         +roam
          )
        ;; perl              ; write code no one else can comprehend
        ;; php               ; perl's insecure younger brother
