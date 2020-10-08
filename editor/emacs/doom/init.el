3768554101cb5ac2772ccfdfb6b9ae450a566488
Eduardo Ruiz Macias <eduarbo@gmail.com>
2020-10-08 02:24:01 -0500

    emacs: support multi languages in the spellchecker

--- editor/emacs/doom/init.el
+++ editor/emacs/doom/init.el
@@ -85,7 +85,11 @@
        (syntax           ; tasing you for every semicolon you forget
          +childframe
          )
-       spell             ; tasing you for misspelling mispelling
+       (spell            ; tasing you for misspelling mispelling
+         +flyspell       ; allow me to use hunspell instead of aspell
+         +hunspell       ; has support for multi dictionaries
+         +everywhere     ; spellcheck comments and strings in the code
+         )
        grammar           ; tasing grammar mistake every you make
 
        :tools
