2608ab7c25956b14ed4bf27dc1440e96692e3b39
Eduardo Ruiz Macias <eduarbo@gmail.com>
2021-01-07 00:38:48 -0600

    prefer to install pyenv with brew

--- dev/python/env.bash
+++ dev/python/env.bash
@@ -5,6 +5,4 @@ export PYLINTRC="$XDG_CONFIG_HOME/pylint/pylintrc"
 export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
 export IPYTHONDIR="$XDG_CONFIG_HOME/ipython"
 
-export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
-path=( "$PYENV_ROOT/bin" "${path[@]}" )
 _cache pyenv init - --no-rehash
