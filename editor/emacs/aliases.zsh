edf4157981741ec439c3c4d9e338c772736c0bc3
Eduardo Ruiz Macias <eduarbo@gmail.com>
2021-03-30 13:18:58 -0600

    update emacs aliases

---
+++ editor/emacs/aliases.zsh
@@ -0,0 +1,4 @@
+e()     { pgrep emacs && emacsclient -n "$@" || emacs -nw "$@" }
+ediff() { emacs -nw --eval "(ediff-files \"$1\" \"$2\")"; }
+eman()  { emacs -nw --eval "(switch-to-buffer (man \"$1\"))"; }
+ekill() { emacsclient --eval '(kill-emacs)'; }
