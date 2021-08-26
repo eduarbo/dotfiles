4171886f0a38a7b966e391bba0de8a462315fd47
Eduardo Ruiz Macias <eduarbo@gmail.com>
2021-08-26 14:08:40 -0500

    emacs: remove ace window settings

--- editor/emacs/doom/+modules.el
+++ editor/emacs/doom/+modules.el
@@ -7,19 +7,6 @@
 ;; ▀▀  █▪▀▀▀ ▀█▄▀▪▀▀▀▀▀•  ▀▀▀ .▀▀▀  ▀▀▀  ▀▀▀▀
 
 
-;; ┏━┓┏━╸┏━╸   ╻ ╻╻┏┓╻╺┳┓┏━┓╻ ╻
-;; ┣━┫┃  ┣╸ ╺━╸┃╻┃┃┃┗┫ ┃┃┃ ┃┃╻┃
-;; ╹ ╹┗━╸┗━╸   ┗┻┛╹╹ ╹╺┻┛┗━┛┗┻┛
-
-(after! ace-window
-  (setq aw-ignore-current t))
-
-(custom-set-faces!
-  '(aw-leading-char-face
-    :foreground "white" :background "red"
-    :weight bold :height 2.5 :box (:line-width 10 :color "red")))
-
-
 ;; ┏━╸┏━┓┏┳┓┏━┓┏━┓┏┓╻╻ ╻
 ;; ┃  ┃ ┃┃┃┃┣━┛┣━┫┃┗┫┗┳┛
 ;; ┗━╸┗━┛╹ ╹╹  ╹ ╹╹ ╹ ╹
