280e64a3d0c673ee403ba7a7148a1717c0ac6691
Eduardo Ruiz Macias <eduarbo@gmail.com>
2021-10-05 11:53:21 -0500

    update launcher

--- macos/hammerspoon/config/launcher.lua
+++ macos/hammerspoon/config/launcher.lua
@@ -1,5 +1,13 @@
+-- Keys used in other apps
+-- hjkl: arrow keys
+-- o: Alfred
+-- t: translates selected text with DeepL
+-- u: launches 1Password mini
+-- y: Clipboard history with Alfred
+
 apps = {
     a = "Calendar",
+    -- b = "",
     c = "Digital Color Meter",
     d = "DeepL",
     e = "Emacs",
@@ -7,13 +15,15 @@ apps = {
     g = "Google Chrome",
     i = "kitty",
     m = "Mail",
+    -- n = "",
     p = "Spotify",
     q = "Activity Monitor",
+    r = "Telegram",
     s = "Slack",
-    t = "Telegram",
-    u = "1Password 7",
+    -- v = "",
     w = "WhatsApp",
-    x = "Firefox Developer Edition"
+    x = "Firefox Developer Edition",
+    z = "1Password 7"
 }
 
 for key, app in pairs(apps) do
