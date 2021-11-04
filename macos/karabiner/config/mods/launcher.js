04a9fc684906e0bfe3e431670bea7f7cd5c04d55
Eduardo Ruiz Macias <eduarbo@gmail.com>
2021-11-04 11:21:44 -0600

    Revert "handle launcher with hammerspoon"
    This reverts commit dc51d7cc6ba8a09c6c67e8d3ba2e57f6226425c8.

---
+++ macos/karabiner/config/mods/launcher.js
@@ -0,0 +1,36 @@
+const appsMap = {
+  a: '/System/Applications/Calendar.app',
+  c: '/System/Applications/Utilities/Digital Color Meter.app',
+  d: '/Applications/1Password 7.app',
+  e: '/Applications/Emacs.app',
+  f: '/System/Library/CoreServices/Finder.app',
+  x: '/Applications/Firefox Developer Edition.app',
+  g: '/Applications/Google Chrome.app',
+  i: '/Applications/kitty.app',
+  m: '/System/Applications/Mail.app',
+  p: '/Applications/Spotify.app',
+  r: '/System/Applications/Utilities/Activity Monitor.app',
+  s: '/Applications/Slack.app',
+  t: '/Applications/Telegram.app',
+  w: '/Applications/WhatsApp.app',
+};
+
+const manipulators = Object.entries(appsMap).map(([key, app]) => ({
+  type: 'basic',
+  from: {
+    key_code: key,
+    modifiers: {
+      mandatory: ['right_option'],
+      optional: ['caps_lock'],
+    },
+  },
+  to: [{ shell_command: `open '${app}'` }],
+}));
+
+module.exports = {
+  title: 'Launcher',
+  rules: [{
+    description: 'Launch apps by right option+letters',
+    manipulators,
+  }],
+};
