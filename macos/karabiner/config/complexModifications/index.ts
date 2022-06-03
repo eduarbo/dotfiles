1a80a61ab8fe7e1dcd49e28fb080451d4817f103
Eduardo Ruiz Macias <eduarbo@gmail.com>
2022-06-03 09:43:40 -0500

    karabiner: add meh layer

--- macos/karabiner/config/complexModifications/index.ts
+++ macos/karabiner/config/complexModifications/index.ts
@@ -2,6 +2,7 @@ export * from './main';
 export * from './base';
 export * from './emacs';
 export * from './hyper';
+export * from './meh';
 export * from './launcher';
 export * from './symbols';
 export * from './symnav';
