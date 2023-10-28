21f3a86d6a1f8dc14adafeec3ca4139e21c4e4cb
Eduardo Ruiz Macias <eduarbo@gmail.com>
2023-10-28 00:41:51 -0600

    set indent style to space

--- editor/coding-style/stylelintrc.js
+++ editor/coding-style/stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-	plugins: ['stylelint-prettier'],
-	rules: {
-		'prettier/prettier': true,
-	},
+  plugins: ['stylelint-prettier'],
+  rules: {
+    'prettier/prettier': true,
+  },
 };
