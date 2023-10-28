98e3e4e4c04e43258537e463a489911c6a9ebff0
Eduardo Ruiz Macias <eduarbo@gmail.com>
2023-10-28 00:41:51 -0600

    set indent style to space

--- editor/coding-style/prettierrc.js
+++ editor/coding-style/prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-	arrowParens: 'avoid',
-	singleQuote: true,
-	trailingComma: 'all',
-	printWidth: 100,
-	bracketSpacing: true,
-	semi: false,
-	proseWrap: 'always',
+  arrowParens: 'avoid',
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+  semi: false,
+  proseWrap: 'always',
 };
