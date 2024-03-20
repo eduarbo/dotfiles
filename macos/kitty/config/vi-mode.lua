8591b04f55bc537f1ac78cae8c5b40ce6afd6c47
Eduardo Ruiz Macias <eduarbo@gmail.com>
2024-03-20 03:24:37 -0600

    quit when pressing `q` in visual mode too

--- macos/kitty/config/vi-mode.lua
+++ macos/kitty/config/vi-mode.lua
@@ -12,6 +12,7 @@ local channel = vim.api.nvim_open_term(buf, {})
 vim.api.nvim_chan_send(channel, table.concat(lines, "\r\n"))
 vim.api.nvim_set_current_buf(buf)
 vim.keymap.set("n", "q", "<cmd>qa!<cr>", { silent = true, buffer = buf })
+vim.keymap.set("v", "q", "<cmd>qa!<cr>", { silent = true, buffer = buf })
 vim.api.nvim_create_autocmd("TermEnter", { buffer = buf, command = "stopinsert" })
 vim.defer_fn(function()
   -- go to the end of the terminal buffer
