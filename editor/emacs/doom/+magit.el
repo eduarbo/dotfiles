b8c8ce03557aaf58e93e69d32e0d0ce5f0f061cf
Eduardo Ruiz Macias <eduarbo@gmail.com>
2024-04-26 15:24:20 -0600

    better defaults for magit

--- editor/emacs/doom/+magit.el
+++ editor/emacs/doom/+magit.el
@@ -1,7 +1,10 @@
 ;;; +magit.el -*- lexical-binding: t; -*-
 
-;; Redefine everything
-(after! (magit evil-collection)
+(setq evil-collection-magit-use-z-for-folds t)
+(setq evil-collection-magit-want-horizontal-movement nil)
+
+;; Redefine the entire help popup
+(after! magit
   (transient-define-prefix magit-dispatch ()
     "Invoke a Magit command from a list of available commands."
     :info-manual "(magit)Top"
@@ -36,8 +39,8 @@
       ("M" "Remote"         magit-remote)
       ;; n                → magit-section-forward
       ;; N       reserved → forge-dispatch
-      ("o" "Submodule"      magit-submodule)
-      ("O" "Subtree"        magit-subtree)
+      ;; o                  ↓
+      ("O" "Revert"         magit-revert)
       ;; p                → magit-section-backward
       ("P" "Push"           magit-push)
       ;; q                → magit-mode-bury-buffer
@@ -51,28 +54,27 @@
       ;; u                  ↓
       ;; U                  ↓
       ;; v                  ↓
-      ("V" "Revert"         magit-revert)]
-     [("w" "Apply patches"  magit-am)
-      ("W" "Format patches" magit-patch)
-      ;; x                  ↓
-      ("X" "Reset"          magit-reset)
+      ("w" "Apply patches"  magit-am)
+      ("W" "Format patches" magit-patch)]
+     ;; x                  ↓
+     [("X" "Reset"          magit-reset)
       ("y" "Show Refs"      magit-show-refs)
       ("Y" "Cherries"       magit-cherry)
       ("z" "Stash"          magit-stash)
       ("Z" "Worktree"       magit-worktree)
-      ("<" "Command"        magit-git-command)
-      (">" "Run"            magit-run)
       ("." "Display buffer" magit-display-repository-buffer)
-      ("=" "More context"   magit-diff-more-context)
-      ("-" "Less context"   magit-diff-less-context)
-      ("0" "Reset context"  magit-diff-default-context)
-      ;; TODO find a better binding
-      ("C-t" "Text-mode"    evil-collection-magit-toggle-text-mode)
-      ]]
+      ("`" "Command"        magit-git-command)
+      ("~" "Run"            magit-run)
+      ("'" "Submodule"      magit-submodule)
+      ("\"" "Subtree"       magit-subtree)
+      ("<" "Less context"   magit-diff-less-context)
+      (">" "More context"   magit-diff-more-context)
+      ("=" "Reset context"  magit-diff-default-context)
+      ("\\" "Text-mode"     evil-collection-magit-toggle-text-mode)]]
     ["Applying changes"
      :if-derived magit-mode
      [("a" "Apply"          magit-apply)
-      ("v" "Reverse"        magit-reverse)
+      ("o" "Reverse"        magit-reverse)
       ("x" "Discard"        magit-discard)]
      [("s" "Stage"          magit-stage)
       ("u" "Unstage"        magit-unstage)]
@@ -80,13 +82,60 @@
       ("U" "Unstage all"    magit-unstage-all)]]
     ["Essential commands"
      :if-derived magit-mode
-     [("g"        "Refresh current buffer"   magit-refresh)
-      ("q"        "Bury current buffer"      magit-mode-bury-buffer)
-      ("Q"        "Bury all magit buffers"   +magit/quit-all)
-      ("j"        "Next line"                evil-next-visual-line)
-      ("k"        "Previous line"            evil-previous-visual-line)]
-     [("<space>" " Show diff in other pane"  magit-diff-show-or-scroll-up)
+     [("j"       " Next line"                evil-next-visual-line)
+      ("k"       " Previous line"            evil-previous-visual-line)
+      ("q"       " Bury current buffer"      magit-mode-bury-buffer)
+      ("Q"       " Bury all magit buffers"   +magit/quit-all)
+      ("v"       " Visual line"              evil-visual-line)
+      ("gr"       "Refresh current buffer"   magit-refresh)
+      ("gR"       "Refresh current buffer"   magit-refresh-all)]
+     [("[up]" "    Previous sibling section" magit-section-backward-sibling)
+      ("[down]" "  Next sibling section"     magit-section-forward-sibling)
+      ("[left]" "  Previous section"         magit-section-backward)
+      ("[right]" " Next section"             magit-section-forward)
+      ("<space>" " Show diff in other pane"  magit-diff-show-or-scroll-up)
       ("<tab>" "   Toggle section at point"  magit-section-toggle)
-      ("<return>" "Visit thing at point"     magit-visit-thing)
-      ("C-x m"    "Show all key bindings"    describe-mode)
-      ("C-x i"    "Show Info manual"         magit-info)]]))
+      ("<return>" "Visit thing at point"     magit-visit-thing)]])
+
+  ;; Align bindings in magit mode with the transient's
+  (map! :map magit-mode-map
+        :nv   "o"      #'magit-reverse
+        :nv   "O"      #'magit-revert
+        :nv   "X"      #'magit-reset
+        :nv   "J"      #'magit-status-jump
+
+        :nv   "."      #'magit-display-repository-buffer
+        :nv   "`"      #'magit-git-command
+        :nv   "~"      #'magit-run
+        :nv   "<"      #'magit-diff-less-context
+        :nv   ">"      #'magit-diff-more-context
+        :nv   "="      #'magit-diff-default-context
+
+        :nv   [up]     #'magit-section-backward-sibling
+        :nv   [down]   #'magit-section-forward-sibling
+        :nv   [left]   #'magit-section-backward
+        :nv   [right]  #'magit-section-forward))
+
+;; Override other transients to align them with the help popup
+(setq evil-collection-magit-popup-changes
+      (append
+       (when evil-collection-magit-use-z-for-folds
+         '((magit-dispatch "Z" "%" magit-worktree)
+           (magit-dispatch "z" "Z" magit-stash)))
+       (when evil-collection-magit-want-horizontal-movement
+         '((magit-dispatch "L" "\C-l" magit-log-refresh)
+           (magit-dispatch "l" "L" magit-log)))
+       '((magit-branch "x" "X" magit-branch-reset)
+         (magit-branch "k" "x" magit-branch-delete)
+         (magit-remote "k" "x" magit-remote-remove)
+         ;; FIXME: how to properly handle a popup with a key that appears twice (in
+         ;; `transient-define-prefix' definition)? Currently we rely on:
+         ;; 1. first call to `evil-collection-magit-change-popup-key' changes the first "V"
+         ;;    entry of `magit-revert' (the first entry in `transient-define-prefix'
+         ;;    definition of `magit-revert'), second call changes the second "V".
+         ;; 2. the remapping here are in the same order as in `magit-revert'
+         ;;    definition
+         (magit-revert "V" "O" magit-revert-and-commit)
+         (magit-revert "V" "O" magit-sequencer-continue)
+         (magit-revert "v" "o" magit-revert-no-commit)
+         (magit-tag    "k" "x" magit-tag-delete))))
