9d9f07f222f5f83c0fc724985512e0f873932c01
Eduardo Ruiz Macias <eduarbo@gmail.com>
2024-04-30 14:14:13 -0600

    node adjustments

--- dev/node/env.zsh
+++ dev/node/env.zsh
@@ -1,7 +1,8 @@
 export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
 export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node/repl_history
-
 export NODENV_ROOT=$XDG_DATA_HOME/nodenv
+export NODENV_PLUGINS=$NODENV_ROOT/plugins
+
 path=( $NODENV_ROOT/bin $path )
 
-_cache nodenv init - --no-rehash
+eval "$(nodenv init -)"
