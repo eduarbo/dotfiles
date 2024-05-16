5e85d1d3d6b69450141af7af7b027f2225e7feb4
Eduardo Ruiz Macias <eduarbo@gmail.com>
2024-05-16 11:38:49 -0600

    adjust bindings

--- macos/hammerspoon/config/window.lua
+++ macos/hammerspoon/config/window.lua
@@ -45,56 +45,6 @@ hs.grid.ui.fontName = 'PT Sans Caption'
 local wasPressed = {false, false, false, false}
 local pressed = {false, false, false, false}
 
-local function resizeWindow(arrowKeys)
-    local leftHalf = arrowKeys[1]
-    local rightHalf = arrowKeys[2]
-
-    local rectMap = {
-        [leftHalf] = {0, 0, 0.5, 1},
-        [rightHalf] = {0.5, 0, 0.5, 1},
-        [leftHalf .. rightHalf] = {0, 0, 1, 1} -- full screen
-    }
-
-    for i = 1, #pressed do
-        if pressed[i] then
-            return
-        end
-    end
-
-    local win = hs.window.focusedWindow()
-    if win ~= nil then
-        local keys = ""
-        for i = 1, #wasPressed do
-            if wasPressed[i] then
-                keys = keys .. arrowKeys[i]
-                wasPressed[i] = false
-            end
-        end
-        local rect = rectMap[keys]
-        if rect ~= nil then
-            if rect == "center" then
-                win:centerOnScreen()
-            else
-                win:move(rect)
-            end
-        end
-    end
-end
-
-function window.bindResize(layerMods, arrowKeys)
-    for i = 1, #arrowKeys do
-        local function pressedFn()
-            wasPressed[i] = true
-            pressed[i] = true
-        end
-        local function releasedFn()
-            pressed[i] = false
-            resizeWindow(arrowKeys)
-        end
-        hs.hotkey.bind(layerMods, arrowKeys[i], pressedFn, releasedFn, nil)
-    end
-end
-
 local function getNextScreen(s)
     all = hs.screen.allScreens()
     for i = 1, #all do
@@ -113,6 +63,70 @@ function window.moveToNextScreen()
         if nextScreen then
             win:moveToScreen(nextScreen)
         end
+    end
+end
+
+function window.toggleMaximize()
+    local win = hs.window.focusedWindow()
+    if win ~= nil then
+        if isMaximized then
+            if previousFrame then
+                win:setFrame(previousFrame)
+            end
+            isMaximized = false
+        else
+            previousFrame = win:frame()
+            win:maximize()
+            isMaximized = true
+        end
+    end
+end
+
+local toggleStates = {
+    {0, 0, 1/2, 1},
+    {0, 0, 1/3, 1},
+    {0, 0, 2/3, 1}
+}
+
+local rightToggleStates = {
+    {1/2, 0, 1/2, 1},
+    {2/3, 0, 1/3, 1},
+    {1/3, 0, 2/3, 1}
+}
+
+local toggleIndex = 1
+local rightToggleIndex = 1
+
+local function isWindowInStates(win, states)
+    local winFrame = win:frame()
+    for _, state in ipairs(states) do
+        local stateFrame = hs.geometry.rect(state):fromUnitRect(win:screen():frame())
+        if winFrame:equals(stateFrame) then
+            return true
+        end
+    end
+    return false
+end
+
+function window.toggleLeftSize()
+    local win = hs.window.focusedWindow()
+    if win ~= nil then
+        if not isWindowInStates(win, toggleStates) then
+            toggleIndex = 1
+        end
+        win:move(toggleStates[toggleIndex])
+        toggleIndex = (toggleIndex % #toggleStates) + 1
+    end
+end
+
+function window.toggleRightSize()
+    local win = hs.window.focusedWindow()
+    if win ~= nil then
+        if not isWindowInStates(win, rightToggleStates) then
+            rightToggleIndex = 1
+        end
+        win:move(rightToggleStates[rightToggleIndex])
+        rightToggleIndex = (rightToggleIndex % #rightToggleStates) + 1
     end
 end
 
