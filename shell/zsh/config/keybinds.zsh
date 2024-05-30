f61fa7b87225d2b01e52891394ebbb9d935f11ef
Eduardo Ruiz Macias <eduarbo@gmail.com>
2024-05-29 18:19:42 -0600

    fix bindkeys

--- shell/zsh/config/keybinds.zsh
+++ shell/zsh/config/keybinds.zsh
@@ -4,10 +4,84 @@
 # Very strange!
 export KEYTIMEOUT=15
 
-autoload -U is-at-least
+# Open current prompt in external editor
+autoload -Uz edit-command-line; zle -N edit-command-line
+bindkey '^ ' edit-command-line
 
-# surround
-# --------
+# History search
+# -----------------------------------------------------------
+
+# Color in which to highlight matched, searched text (default bg=17 on 256-color terminals)
+zstyle ":history-search-multi-word" highlight-color "fg=yellow,bold"
+
+# bind UP and DOWN arrow keys
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+bindkey "^R" history-search-multi-word
+
+# FZF
+# -----------------------------------------------------------
+
+bindkey '^T' fzf-history-widget
+bindkey '^O' fzf-cd-widget
+bindkey '^I' fzf-file-widget
+
+# Fancy Ctrl+Z
+# -----------------------------------------------------------
+
+# C-z to toggle current process (background/foreground)
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
+
+# vi-mode
+# -----------------------------------------------------------
+
+bindkey -M viins '^u' backward-kill-line
+bindkey -M viins '^w' backward-kill-word
+bindkey -M viins '^b' backward-word
+bindkey -M viins '^f' forward-word
+bindkey -M viins '^g' push-line-or-edit
+bindkey -M viins '^a' beginning-of-line
+bindkey -M viins '^e' end-of-line
+bindkey -M viins '^d' push-line-or-edit
+
+bindkey -M vicmd '^k' kill-line
+bindkey -M vicmd 'H'  run-help
+
+# Shift + Tab
+bindkey -M viins '^[[Z' reverse-menu-complete
+
+# Vim's C-x C-l in zsh
+history-beginning-search-backward-then-append() {
+  zle history-beginning-search-backward
+  zle vi-add-eol
+}
+zle -N history-beginning-search-backward-then-append
+bindkey -M viins '^x^l' history-beginning-search-backward-then-append
+
+# Fix the DEL key
+bindkey -M vicmd "^[[3~" delete-char
+bindkey "^[[3~" delete-char
+
+# Fix vimmish ESC
+bindkey -sM vicmd '^[' '^G'
+bindkey -rM viins '^X'
+bindkey -M viins '^X,' _history-complete-newer \
+  '^X/' _history-complete-older \
+  '^X`' _bash_complete-word
+
+# Surround
+# -----------------------------------------------------------
 
 autoload -Uz surround
 zle -N delete-surround surround
@@ -16,6 +90,8 @@ zle -N change-surround surround
 bindkey -a cs change-surround
 bindkey -a ds delete-surround
 bindkey -a ys add-surround
+
+autoload -U is-at-least
 
 # <5.0.8 doesn't have visual map
 if is-at-least 5.0.8; then
@@ -36,53 +112,9 @@ if is-at-least 5.0.8; then
   done
 fi
 
-# Open current prompt in external editor
-autoload -Uz edit-command-line; zle -N edit-command-line
-bindkey '^ ' edit-command-line
-
-# vi-mode
-# -------
-
-# bindkey -M viins ' ' magic-space
-bindkey -M viins '^u' backward-kill-line
-bindkey -M viins '^w' backward-kill-word
-bindkey -M viins '^b' backward-word
-bindkey -M viins '^f' forward-word
-bindkey -M viins '^g' push-line-or-edit
-bindkey -M viins '^a' beginning-of-line
-bindkey -M viins '^e' end-of-line
-bindkey -M viins '^d' push-line-or-edit
-
-bindkey -M vicmd '^k' kill-line
-bindkey -M vicmd 'H'  run-help
-
-# Shift + Tab
-bindkey -M viins '^[[Z' reverse-menu-complete
-
-# history-search-multi-word
-# -------------------------
-
-bindkey -M vicmd '?'  history-search-multi-word
-bindkey '^R' history-search-multi-word
-
-# bind UP and DOWN arrow keys
-bindkey '^[[A' history-search-multi-word-backwards
-bindkey '^[[B' history-search-multi-word
-
-# C-z to toggle current process (background/foreground)
-fancy-ctrl-z () {
-  if [[ $#BUFFER -eq 0 ]]; then
-    BUFFER="fg"
-    zle accept-line
-  else
-    zle push-input
-    zle clear-screen
-  fi
-}
-zle -N fancy-ctrl-z
-bindkey '^Z' fancy-ctrl-z
-
 # Completing words in buffer in tmux
+# -----------------------------------------------------------
+
 if [ -n "$TMUX" ]; then
   _tmux_pane_words() {
     local expl
@@ -105,22 +137,3 @@ if [ -n "$TMUX" ]; then
   zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' ignore-line current
   zstyle ':completion:tmux-pane-words-anywhere:*' matcher-list 'b:=* m:{A-Za-z}={a-zA-Z}'
 fi
-
-# Vim's C-x C-l in zsh
-history-beginning-search-backward-then-append() {
-  zle history-beginning-search-backward
-  zle vi-add-eol
-}
-zle -N history-beginning-search-backward-then-append
-bindkey -M viins '^x^l' history-beginning-search-backward-then-append
-
-# Fix the DEL key
-bindkey -M vicmd "^[[3~" delete-char
-bindkey "^[[3~" delete-char
-
-# Fix vimmish ESC
-bindkey -sM vicmd '^[' '^G'
-bindkey -rM viins '^X'
-bindkey -M viins '^X,' _history-complete-newer \
-  '^X/' _history-complete-older \
-  '^X`' _bash_complete-word
