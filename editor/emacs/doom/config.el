e4e64642a918711d59b3ac8daafa44b730481e1e
Eduardo Ruiz Macias <eduarbo@gmail.com>
2025-02-06 21:40:44 -0600

    move my personal info to env vars

--- editor/emacs/doom/config.el
+++ editor/emacs/doom/config.el
@@ -5,8 +5,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Eduardo Ruiz"
-      user-mail-address "eduarbo@gmail.com")
+(setq user-full-name (getenv "USER_FULL_NAME")
+      user-mail-address (getenv "USER_EMAIL"))
 
 (setq doom-leader-key ",")
 (setq doom-localleader-key "s-m")
