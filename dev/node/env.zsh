e4574fd41b8825d49c34d43501ef0cb1fd9330f4
Your Name <you@example.com>
2025-06-08 01:04:31 -0600

    add support to bun

--- dev/node/env.zsh
+++ dev/node/env.zsh
@@ -2,5 +2,6 @@ export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
 export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node/repl_history
 export NODENV_ROOT=$XDG_DATA_HOME/nodenv
 export NODENV_PLUGINS=$NODENV_ROOT/plugins
+export BUN_INSTALL="$HOME/.bun"
 
-path=( $NODENV_ROOT/bin $path )
+path=($NODENV_ROOT/bin $BUN_INSTALL/bin $path)
