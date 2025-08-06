9b61c9663adaf11676a67f41e55e952eb80cc6c0
Your Name <you@example.com>
2025-08-06 12:36:04 -0600

    setup pnpm

--- dev/node/env.zsh
+++ dev/node/env.zsh
@@ -3,5 +3,6 @@ export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node/repl_history
 export NODENV_ROOT=$XDG_DATA_HOME/nodenv
 export NODENV_PLUGINS=$NODENV_ROOT/plugins
 export BUN_INSTALL="$HOME/.bun"
+export PNPM_HOME="${XDG_DATA_HOME}/pnpm"
 
-path=($NODENV_ROOT/bin $BUN_INSTALL/bin $path)
+path=($NODENV_ROOT/bin $BUN_INSTALL/bin $PNPM_HOME $path)
