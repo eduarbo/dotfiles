30dd8f5cc27ac395ff76446634da2e63421d9bbd
Eduardo Ruiz Macias <eduarbo@gmail.com>
2026-01-26 00:26:53 -0600

    macos: add android sdk and platform-tools to path

--- macos/apps/env.zsh
+++ macos/apps/env.zsh
@@ -1,1 +1,4 @@
 path=( "/Applications/Docker.app/Contents/Resources/bin/" $path )
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+path=( "$ANDROID_HOME/platform-tools" "$ANDROID_HOME/emulator" $path)
